659fab82f56edc942ebabfcff57ba6f0b5008788
pedrogodoy <pedrogh.godoy12@gmail.com>
2021-03-10 21:55:27 -0400

    Create account and get statement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+/*
+  cpf - string
+  name - string
+  id - uuid
+  statement - []
+*/
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some((c) => c.cpf === cpf);
+
+  if(customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
+
+  const id = uuidv4();
+
+  customers.push({ cpf, name, id, statement: [] });
+  return res.status(201).send();
+
+});
+
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
+
+  return res.json(customer.statement);
+});
+
+
+
 
 app.listen(3333);
