60ee63f0661c4600978f2dad56639b27d5fece67
pedrogodoy <pedrogh.godoy12@gmail.com>
2021-03-11 06:47:48 -0400

    Deposit route

--- src/index.js
+++ src/index.js
@@ -44,6 +44,23 @@ app.get("/statement", verifyIfExistsAccountCPF,(req, res) => {
   return res.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+});
+
 
 
 
